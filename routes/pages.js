b363e9d5e55f76470ae26a798a5028894813142f
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-05-26 17:29:28 +0300

    page rendering for web app

--- routes/pages.js
+++ routes/pages.js
@@ -1,9 +1,41 @@
 const express = require("express");
+const authController = require('../controllers/auth');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index');
+router.get('/', authController.isLoggedIn, (req, res) => {
+    res.render('index', {
+      user: req.user
+    });
 });
+  
+router.get('/register', (req, res) => {
+res.render('register');
+});
+
+router.get('/login', (req, res) => {
+res.render('login');
+});
+
+// router.get('/add', (req, res) => {
+// res.render('profile');
+// });
+  
+router.get('/profile', authController.isLoggedIn, (req, res) => {
+    console.log(req.user);
+    if( req.user ) {
+        res.render('profile', {
+            user: req.user
+        });
+    } else {
+        res.redirect('/login');
+    }
+
+});
+
+router.get('/reports', (req, res) => {
+    res.render('reports');
+});
+    
 
 module.exports = router;
